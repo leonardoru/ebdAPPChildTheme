c1c06ca04befb97275177e3415d93e49a5cc69cc
leonardoru <leorueda777@gmail.com>
2021-06-19 17:38:38 -0700

    Enque main styles on functions file

--- functions.php
+++ functions.php
@@ -1,1 +1,12 @@
-<?php include_once "acf-fields.php"; ?>
+<?php 
+
+# Define some useful vars
+$child_theme_root = get_stylesheet_directory_uri()
+
+# Include the ACF Field definition file
+include_once "acf-fields.php"; 
+
+# Enque main styles of the child theme
+wp_enqueue_style("ebdAPPStyles", $child_theme_root . "/src/css/ebdAPPStyles.css"
+
+?>
